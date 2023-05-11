a6a53f3371fe2fffc331cada0e9231648bb56433
samwu <49509248+samwuwu@users.noreply.github.com>
2023-05-11 20:44:23 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,11 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = 'feff08c6-515e-4682-9c72-34fd56c14e3b'
+export const API_KEY = 'app-vn1yuBpY5DBeCdFAi7wGinZy'
 
 export const APP_INFO: AppInfo = {
-  "title": 'Text Generator APP',
-  "description": 'App description',
-  "copyright": '',
+  "title": 'emoji表情自动添加',
+  "description": '👉 可以将你发送的📝文本内容，自动添加😊emoji表情符号，提高🤩趣味性。',
+  "copyright": 'Sam Wu',
   "privacy_policy": '',
   "default_language": 'zh-Hans'
 }
